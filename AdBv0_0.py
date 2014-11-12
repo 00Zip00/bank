c130e0f2f9171512a940ae39ed086e579b27c01c
Claudio <el_zip@hotmail.it>
2014-11-12 22:04:36 +0100

    Update AdBv0_0.py

--- AdBv0_0.py
+++ AdBv0_0.py
@@ -4,5 +4,8 @@ print "Algoritmo del Banchiere versione",vers
 n=input('inserisci il numero di risorse ')
 m=input('inserisci il numero di processi ')
 
+for i in range (0,n)
+  sys_max[i]=input('inserisci il quantitativo massimo della risorsa ',n)
+
 
 
